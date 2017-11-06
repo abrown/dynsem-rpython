96488296eb086d3c06c8d54de704d4ee8d81afae
abrown <andrew.brown@intel.com>
2017-11-05 18:53:14 -0800

    Add dynamic dispatch benchmark

---
+++ src/benchmark/method.py
@@ -0,0 +1,67 @@
+import sys  # for print
+
+# So that you can still run this module under standard CPython, I add this
+# import guard that creates a dummy class instead.
+try:
+    from rpython.rlib.jit import JitDriver, elidable, promote
+except ImportError:
+    class JitDriver(object):
+        def __init__(self,**kw): pass
+        def jit_merge_point(self,**kw): pass
+    def elidable(func): return func
+    def promote(x): return x
+
+
+jitdriver = JitDriver(greens=[], reds='auto')
+
+
+def jitpolicy(driver):
+    from rpython.jit.codewriter.policy import JitPolicy
+    return JitPolicy()
+
+
+class Exp:
+    def call(self, operand):
+        return 0
+
+    def __str__(self):
+        return self.__class__.__name__
+
+
+class Add1Exp(Exp):
+    def call(self, operand):
+        return operand + 1
+
+
+class Add2Exp(Exp):
+    def call(self, operand):
+        return operand + 2
+
+
+class Add3Exp(Exp):
+    def call(self, operand):
+        return operand + 3
+
+
+def main(argv):
+    if not len(argv) == 2:
+        raise RuntimeError("Expect one numeric argument passed, e.g. program 1000");
+
+    program = [Add1Exp(), Add3Exp(), Add2Exp()]
+    total = int(argv[1])
+    accumulator = 0
+    for i in range(0, total):
+        jitdriver.jit_merge_point()
+        for e in program:
+            accumulator = e.call(accumulator)
+
+    print("%d" % accumulator)
+    return 0
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
+
+def target(*args):
+    return main, None
---
+++ src/benchmark/switch.py
@@ -0,0 +1,55 @@
+import sys  # for print
+
+# So that you can still run this module under standard CPython, I add this
+# import guard that creates a dummy class instead.
+try:
+    from rpython.rlib.jit import JitDriver, elidable, promote
+except ImportError:
+    class JitDriver(object):
+        def __init__(self,**kw): pass
+        def jit_merge_point(self,**kw): pass
+    def elidable(func): return func
+    def promote(x): return x
+
+
+jitdriver = JitDriver(greens=[], reds='auto')
+
+
+def jitpolicy(driver):
+    from rpython.jit.codewriter.policy import JitPolicy
+    return JitPolicy()
+
+
+def interpret(code, operand):
+    if code == 1:
+        return operand + 1
+    elif code == 2:
+        return operand + 2
+    elif code == 3:
+        return operand + 3
+    else:
+        raise NotImplementedError()
+
+
+def main(argv):
+    if not len(argv) == 2:
+        raise RuntimeError("Expect one numeric argument passed, e.g. program 1000");
+
+    program = [1, 3, 2]
+    total = int(argv[1])
+    accumulator = 0
+    for i in range(0, total):
+        jitdriver.jit_merge_point()
+        for c in program:
+            accumulator = interpret(c, accumulator)
+
+    print("%d" % accumulator)
+    return 0
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
+
+def target(*args):
+    return main, None
