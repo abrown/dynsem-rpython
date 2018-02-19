f7c2c6b9279fb607cbe7e26d9a68c20262cd10cf
abrown <andrew.brown@intel.com>
2018-02-19 11:17:00 -0800

    Fix handmade interpreter

--- src/compile/handmade.py
+++ src/compile/handmade.py
@@ -47,7 +47,7 @@ def get_location(hashed_term, interpreter):
     return "%d" % hashed_term
 
 
-jitdriver = JitDriver(greens=['hashed_term', 'interpreter'], reds=['term'], get_printable_location=get_location)
+jitdriver = JitDriver(greens=['hashed_term', 'interpreter'], reds='auto', get_printable_location=get_location)
 
 
 def jitpolicy(driver):
@@ -80,121 +80,111 @@ class Interpreter:
         self.environment = ListBackedMap()
 
     def interpret(self, term):
+        if self.debug > 1:
+            print(term)
+
         while isinstance(term, ApplTerm):
-            jitdriver.jit_merge_point(hashed_term=term.hash, interpreter=self, term=term)
-
-            if self.debug:
-                print(term)
-                
-            term = self.eval(term)
-
-            if isinstance(term, ApplTerm) and term.has_loop:
-                if self.debug:
-                    print("looping")
-                jitdriver.can_enter_jit(hashed_term=term.hash, interpreter=self, term=term)
-
-        return term
-
-    def eval(self, term):
-        if term.name_hash == WHILE:
-            # while(cond, then) --> while2(cond, value, then) where cond --> value
-            cond = term.args[0]
-            then = term.args[1]
-            value = self.interpret(cond)
-            return ApplTerm('while2', [cond, value, then], WHILE2)
-        elif term.name_hash == WHILE2:
-            if term.args[1].number == 0:
-                # while2(cond, 0, then) --> 0
-                return IntTerm(0)
-            else:
-                # while2(cond, value, then) --> while(cond, then) where then --> ignored
+            if term.name_hash == WHILE:
+                # while(cond, then) --> result where case cond { 0 => 0 => result, otherwise then --> ignored; while(cond, then) => result }
                 cond = term.args[0]
-                then = term.args[2]
-                ignored = self.interpret(then)
-                return ApplTerm('while', [cond, then], WHILE, True)
-        elif term.name_hash == BLOCK:
-            # block([x | xs]) --> block(xs) where x --> y
-            if len(term.args) == 0:
-                return None
-            list = term.args[0]
-            assert isinstance(list, ListTerm)
-            if len(list.items) == 0:
-                return None
-            x = list.items[0]
-            y = self.interpret(x)
-            if len(list.items) == 1:
-                return ApplTerm('block', [ListTerm()], BLOCK)
+                then = term.args[1]
+                jitdriver.jit_merge_point(hashed_term=term.hash, interpreter=self)
+                value = self.interpret(cond)
+                if value.number == 0:
+                    return IntTerm(0)
+                else:
+                    ignored = self.interpret(then)
+                    # no instantiation of while(cond, then)
+                    continue
+            elif term.name_hash == BLOCK:
+                # block([x | xs]) --> block(xs) where x --> y
+                if len(term.args) == 0:
+                    return None  # special block() case TODO remove
+                list = term.args[0]
+                assert isinstance(list, ListTerm)
+                if len(list.items) == 0:
+                    return None
+                x = list.items[0]
+                y = self.interpret(x)
+                new_term = ApplTerm(term.name, [ListTerm(list.items[1:])], term.name_hash)
+                return self.interpret(new_term)
+            elif term.name_hash == ASSIGN:
+                # E |- assign(x, v) --> {x |--> v, E}
+                x = term.args[0]
+                assert isinstance(x, VarTerm)
+                v = self.interpret(term.args[1])
+                if x.index < 0:
+                    x.index = self.environment.locate(x.name)
+                self.environment.put(x.index, v)
+                if self.debug:
+                    print("\tassign %s := %s" % (x.to_string(), v.to_string()))
+                return IntTerm(0)
+            elif term.name_hash == RETRIEVE:
+                # E |- retrieve(x) --> E[x]
+                x = term.args[0]
+                assert isinstance(x, VarTerm)
+                if x.index < 0:
+                    x.index = self.environment.locate(x.name)
+                if self.debug:
+                    print("\tretrieve %s := %s" % (x.to_string(), self.environment.get(x.index).to_string()))
+                return self.environment.get(x.index)
+            elif term.name_hash == IFZ:
+                # ifz(cond, then, else) --> result where cond --> cond2; case cond2 of {0 => result => else otherwise => result => then}
+                cond = term.args[0]
+                then = term.args[1]
+                else_ = term.args[2]
+                cond_ = self.interpret(cond)
+                if cond_.number == 0:
+                    result = self.interpret(else_)
+                else:
+                    result = self.interpret(then)
+                return result
+            elif term.name_hash == WRITE:
+                # native
+                s = self.interpret(term.args[0])
+                print(s.to_string())
+                return IntTerm(0)
+            elif term.name_hash == ADD:
+                # native
+                x = self.interpret(term.args[0])
+                y = self.interpret(term.args[1])
+                z = x.number + y.number
+                if self.debug:
+                    print("\t%d + %d = %d" % (x.number, y.number, z))
+                return IntTerm(z)
+            elif term.name_hash == SUB:
+                # native
+                x = self.interpret(term.args[0])
+                y = self.interpret(term.args[1])
+                z = x.number - y.number
+                if self.debug:
+                    print("\t%d - %d = %d" % (x.number, y.number, z))
+                return IntTerm(z)
+            elif term.name_hash == MUL:
+                # native
+                x = self.interpret(term.args[0])
+                y = self.interpret(term.args[1])
+                z = x.number * y.number
+                if self.debug:
+                    print("\t%d * %d = %d" % (x.number, y.number, z))
+                return IntTerm(z)
+            elif term.name_hash == DIV:
+                # native
+                x = self.interpret(term.args[0])
+                y = self.interpret(term.args[1])
+                z = x.number // y.number
+                if self.debug:
+                    print("\t%d // %d = %d" % (x.number, y.number, z))
+                return IntTerm(z)
+            elif term.name_hash == LEQ:
+                # native
+                x = self.interpret(term.args[0])
+                y = self.interpret(term.args[1])
+                z = int(x.number <= y.number)
+                if self.debug:
+                    print("\t%d <= %d = %d" % (x.number, y.number, z))
+                return IntTerm(z)
             else:
-                xs = list.items[1:]
-                return ApplTerm('block', [ListTerm(xs)], BLOCK)
-        elif term.name_hash == ASSIGN:
-            # E |- assign(x, v) --> {x |--> v, E}
-            x = term.args[0]
-            assert isinstance(x, VarTerm)
-            v = self.interpret(term.args[1])
-            if x.index < 0:
-                x.index = self.environment.locate(x.name)
-            self.environment.put(x.index, v)
-            # print("\tassign %s := %s" % (x.to_string(), v.to_string()))
-            return IntTerm(0)
-        elif term.name_hash == RETRIEVE:
-            # E |- retrieve(x) --> E[x]
-            x = term.args[0]
-            assert isinstance(x, VarTerm)
-            if x.index < 0:
-                x.index = self.environment.locate(x.name)
-            # print("\tretrieve %s := %s" % (x.to_string(), self.environment.get(x.index).to_string()))
-            return self.environment.get(x.index)
-        elif term.name_hash == IFZ:
-            # ifz(cond, then, else) --> result where cond --> cond2; case cond2 of {0 => result => else otherwise => result => then}
-            cond = term.args[0]
-            then = term.args[1]
-            else_ = term.args[2]
-            cond_ = self.interpret(cond)
-            if cond_.number == 0:
-                result = else_
-            else:
-                result = then
-            return result
-        elif term.name_hash == WRITE:
-            # native
-            s = self.interpret(term.args[0])
-            print(s.to_string())
-            return IntTerm(0)
-        elif term.name_hash == ADD:
-            # native
-            x = self.interpret(term.args[0])
-            y = self.interpret(term.args[1])
-            z = x.number + y.number
-            # print("\t%d + %d = %d" % (x.number, y.number, z))
-            return IntTerm(z)
-        elif term.name_hash == SUB:
-            # native
-            x = self.interpret(term.args[0])
-            y = self.interpret(term.args[1])
-            z = x.number - y.number
-            # print("\t%d - %d = %d" % (x.number, y.number, z))
-            return IntTerm(z)
-        elif term.name_hash == MUL:
-            # native
-            x = self.interpret(term.args[0])
-            y = self.interpret(term.args[1])
-            z = x.number * y.number
-            # print("\t%d * %d = %d" % (x.number, y.number, z))
-            return IntTerm(z)
-        elif term.name_hash == DIV:
-            # native
-            x = self.interpret(term.args[0])
-            y = self.interpret(term.args[1])
-            z = x.number // y.number
-            # print("\t%d // %d = %d" % (x.number, y.number, z))
-            return IntTerm(z)
-        elif term.name_hash == LEQ:
-            # native
-            x = self.interpret(term.args[0])
-            y = self.interpret(term.args[1])
-            z = int(x.number <= y.number)
-            # print("\t%d <= %d = %d" % (x.number, y.number, z))
-            return IntTerm(z)
+                raise NotImplementedError
         else:
-            raise NotImplementedError
+            return term
